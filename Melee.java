7833b312080d993b07496153da5f64f0d89d37ff
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-20 12:31:55 -0400

    Update Melee.java

--- Melee.java
+++ Melee.java
@@ -1,2 +1,19 @@
+import java.awt.event.KeyEvent;
+
 public class Melee extends Player {
+    public Melee() {
+        super();
+    }
+    public void keyPressed(KeyEvent key) {
+        if (key.getKeyCode() == key.VK_W) forward = true;
+        if (key.getKeyCode() == key.VK_S) back = true;
+        if (key.getKeyCode() == key.VK_A) left = true;
+        if (key.getKeyCode() == key.VK_D) right = true;
+    }
+    public void keyReleased(KeyEvent key) {
+        if (key.getKeyCode() == key.VK_W) forward = false;
+        if (key.getKeyCode() == key.VK_S) back = false;
+        if (key.getKeyCode() == key.VK_A) left = false;
+        if (key.getKeyCode() == key.VK_D) right = false;
+    }
 }
