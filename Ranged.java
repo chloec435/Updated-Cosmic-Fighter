1d05fa7a74f23e06bebd775085c92a12c340f518
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-20 12:32:03 -0400

    Update Ranged.java

--- Ranged.java
+++ Ranged.java
@@ -1,2 +1,19 @@
+import java.awt.event.KeyEvent;
+
 public class Ranged extends Player {
+    public Ranged() {
+        super();
+    }
+    public void keyPressed(KeyEvent key) {
+        if (key.getKeyCode() == key.VK_KP_UP) forward = true;
+        if (key.getKeyCode() == key.VK_KP_DOWN) back = true;
+        if (key.getKeyCode() == key.VK_KP_LEFT) left = true;
+        if (key.getKeyCode() == key.VK_KP_RIGHT) right = true;
+    }
+    public void keyReleased(KeyEvent key) {
+        if (key.getKeyCode() == key.VK_KP_UP) forward = false;
+        if (key.getKeyCode() == key.VK_KP_DOWN) back = false;
+        if (key.getKeyCode() == key.VK_KP_LEFT) left = false;
+        if (key.getKeyCode() == key.VK_KP_RIGHT) right = false;
+    }
 }
