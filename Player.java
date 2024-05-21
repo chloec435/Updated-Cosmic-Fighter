c1620b6bee2fe7c4ba08fc8f7ceb84986c467590
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-21 12:31:10 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,15 +3,31 @@ public class Player {
     protected boolean forward,back,left,right;
     protected boolean normalAttack,AOE,ultimate;
     protected double moveSpeed = 0.5;
-    protected int x,y,xDir,yDir;
+    protected float xPos,yPos;
+    protected float xDir = 1, yDir = 0;
+    protected int[][] map;
     public Player(){}
-    public Player(String type) {
+    public Player(String type, int[][] mapping) {
         type(type);
+        map = mapping;
     }
     public void type(String type) {
         if (type == "melee") new Melee();
         if (type == "ranged") new Ranged();
         if (type == "support") new Support();
     }
-    
+    public void movement(int x, int y) {
+        if (forward) {
+            xPos = 
+        }
+        if (back) {
+
+        }
+        if (left) {
+
+        }
+        if (right) {
+
+        }
+    }
 }
