2d3897f4de12dd63ecdc9f1fe05cc667d618a2c4
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-24 13:00:42 -0400

    Update Ranged.java

--- Ranged.java
+++ Ranged.java
@@ -5,15 +5,15 @@ public class Ranged extends Player {
         super();
     }
     public void keyPressed(KeyEvent key) {
-        if (key.getKeyCode() == key.VK_KP_UP) forward = true;
-        if (key.getKeyCode() == key.VK_KP_DOWN) back = true;
-        if (key.getKeyCode() == key.VK_KP_LEFT) left = true;
-        if (key.getKeyCode() == key.VK_KP_RIGHT) right = true;
+        if (key.getKeyCode() == key.VK_NUMPAD8) forward = true;
+        if (key.getKeyCode() == key.VK_NUMPAD2) back = true;
+        if (key.getKeyCode() == key.VK_NUMPAD4) left = true;
+        if (key.getKeyCode() == key.VK_NUMPAD6) right = true;
     }
     public void keyReleased(KeyEvent key) {
-        if (key.getKeyCode() == key.VK_KP_UP) forward = false;
-        if (key.getKeyCode() == key.VK_KP_DOWN) back = false;
-        if (key.getKeyCode() == key.VK_KP_LEFT) left = false;
-        if (key.getKeyCode() == key.VK_KP_RIGHT) right = false;
+        if (key.getKeyCode() == key.VK_NUMPAD8) forward = false;
+        if (key.getKeyCode() == key.VK_NUMPAD2) back = false;
+        if (key.getKeyCode() == key.VK_NUMPAD4) left = false;
+        if (key.getKeyCode() == key.VK_NUMPAD6) right = false;
     }
 }
