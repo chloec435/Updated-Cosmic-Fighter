f5e054f8667b6bcbaf0f1b636128825e714ef7ba
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-28 12:08:22 -0400

    Update Support.java

--- Support.java
+++ Support.java
@@ -9,11 +9,17 @@ public class Support extends Player {
         if (key.getKeyCode() == key.VK_KP_DOWN) back = true;
         if (key.getKeyCode() == key.VK_KP_LEFT) left = true;
         if (key.getKeyCode() == key.VK_KP_RIGHT) right = true;
+        if (key.getKeyCode() == key.VK_J) ultimate = true;
+        if (key.getKeyCode() == key.VK_K) skill = true;
+        if (key.getKeyCode() == key.VK_L) normal = true;
     }
     public void keyReleased(KeyEvent key) {
         if (key.getKeyCode() == key.VK_KP_UP) forward = false;
         if (key.getKeyCode() == key.VK_KP_DOWN) back = false;
         if (key.getKeyCode() == key.VK_KP_LEFT) left = false;
         if (key.getKeyCode() == key.VK_KP_RIGHT) right = false;
+        if (key.getKeyCode() == key.VK_J) ultimate = false;
+        if (key.getKeyCode() == key.VK_K) skill = false;
+        if (key.getKeyCode() == key.VK_L) normal = false;
     }
 }
