21cab3a2bfbe5aeda8d89371cfb014849680c4d0
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-28 12:08:15 -0400

    Update Ranged.java

--- Ranged.java
+++ Ranged.java
@@ -9,11 +9,17 @@ public class Ranged extends Player {
         if (key.getKeyCode() == key.VK_NUMPAD2) back = true;
         if (key.getKeyCode() == key.VK_NUMPAD4) left = true;
         if (key.getKeyCode() == key.VK_NUMPAD6) right = true;
+        if (key.getKeyCode() == key.VK_NUMPAD9) ultimate = true;
+        if (key.getKeyCode() == key.VK_NUMPAD7) skill = true;
+        if (key.getKeyCode() == key.VK_NUMPAD0) normal = true;
     }
     public void keyReleased(KeyEvent key) {
         if (key.getKeyCode() == key.VK_NUMPAD8) forward = false;
         if (key.getKeyCode() == key.VK_NUMPAD2) back = false;
         if (key.getKeyCode() == key.VK_NUMPAD4) left = false;
         if (key.getKeyCode() == key.VK_NUMPAD6) right = false;
+        if (key.getKeyCode() == key.VK_NUMPAD9) ultimate = false;
+        if (key.getKeyCode() == key.VK_NUMPAD7) skill = false;
+        if (key.getKeyCode() == key.VK_NUMPAD0) normal = false;
     }
 }
