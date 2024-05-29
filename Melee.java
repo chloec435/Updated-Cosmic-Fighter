a35a6791eeef94b9601ffddb38e586473dc14e00
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-05-29 12:33:24 -0400

    Update Melee.java

--- Melee.java
+++ Melee.java
@@ -1,26 +1,29 @@
 import java.awt.event.KeyEvent;
 
 public class Melee extends Player {
+    private int code;
     public Melee() {
         super();
         movement();
     }
     public void keyPressed(KeyEvent key) {
-        if (key.getKeyCode() == key.VK_W) forward = true;
-        if (key.getKeyCode() == key.VK_S) back = true;
-        if (key.getKeyCode() == key.VK_A) left = true;
-        if (key.getKeyCode() == key.VK_D) right = true;
-        if (key.getKeyCode() == key.VK_Q) ultimate = true;
-        if (key.getKeyCode() == key.VK_E) skill = true;
-        if (key.getKeyCode() == key.VK_SPACE) normal = true;
+        code = key.getKeyCode();
+        if (code == key.VK_W) forward = true;
+        if (code == key.VK_S) back = true;
+        if (code == key.VK_A) left = true;
+        if (code == key.VK_D) right = true;
+        if (code == key.VK_Q) ultimate = true;
+        if (code == key.VK_E) skill = true;
+        if (code == key.VK_SPACE) normal = true;
     }
     public void keyReleased(KeyEvent key) {
-        if (key.getKeyCode() == key.VK_W) forward = false;
-        if (key.getKeyCode() == key.VK_S) back = false;
-        if (key.getKeyCode() == key.VK_A) left = false;
-        if (key.getKeyCode() == key.VK_D) right = false;
-        if (key.getKeyCode() == key.VK_Q) ultimate = false;
-        if (key.getKeyCode() == key.VK_E) skill = false;
-        if (key.getKeyCode() == key.VK_SPACE) normal = false;
+        code = key.getKeyCode();
+        if (code == key.VK_W) forward = false;
+        if (code == key.VK_S) back = false;
+        if (code == key.VK_A) left = false;
+        if (code == key.VK_D) right = false;
+        if (code == key.VK_Q) ultimate = false;
+        if (code == key.VK_E) skill = false;
+        if (code == key.VK_SPACE) normal = false;
     }
 }
