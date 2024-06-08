feba5ac7633a36654727d243d1efc4560b97a8bc
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-08 16:00:32 -0400

    Update Startup.java

--- Startup.java
+++ Startup.java
@@ -1,33 +1,33 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
-public class Game extends JFrame {
+public class Startup extends JFrame {
     private int[] map;
     private int[] pixels;
     private BufferedImage[] images;
-    public Game() {
+    private JButton play;
+    private JButton help;
+    public Startup() {
         startingScreen();
+        play();
         setVisible(true);
     }
     public void startingScreen() {
-        setSize(1720,960);
+        setSize(1920,1080);
         SpringLayout layout = new SpringLayout();
         setLayout(layout);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        JButton menu = new JButton(); //creating menu button
-        menu.setPreferredSize(new Dimension(50,50));
-        menu.setBackground(Color.WHITE);
-        ImageIcon threeDash = new ImageIcon(new ImageIcon("Images/Three dash.png")
-                .getImage().getScaledInstance(40,40, Image.SCALE_SMOOTH));
-        menu.setIcon(threeDash);
-        menu.setBorder(new LineBorder(Color.BLACK, 3));
-        JButton play = new JButton(); //creating play button
+        play = new JButton(); //creating play button
         play.setPreferredSize(new Dimension(500,100));
         ImageIcon PLAY = new ImageIcon(new ImageIcon("Images/Play.png")
                 .getImage().getScaledInstance(500,100, Image.SCALE_SMOOTH));
@@ -35,7 +35,7 @@ public class Game extends JFrame {
         play.setBorderPainted(false);
         play.setOpaque(false);
         play.setContentAreaFilled(false);
-        JButton help = new JButton(); //creating help button
+        help = new JButton(); //creating help button
         help.setPreferredSize(new Dimension(500,100));
         ImageIcon HELP = new ImageIcon(new ImageIcon("Images/Help.png")
                 .getImage().getScaledInstance(500,100, Image.SCALE_SMOOTH));
@@ -43,25 +43,42 @@ public class Game extends JFrame {
         help.setBorderPainted(false);
         help.setOpaque(false);
         help.setContentAreaFilled(false);
-        add(menu);
         add(play);
         add(help);
         //setting layout for buttons
-        layout.putConstraint(SpringLayout.EAST, menu, -5, SpringLayout.EAST, getContentPane());
-        layout.putConstraint(SpringLayout.NORTH, menu, 5, SpringLayout.NORTH, getContentPane());
         layout.putConstraint(SpringLayout.WEST, play, (getWidth()-500)/2, SpringLayout.WEST, getContentPane());
-        layout.putConstraint(SpringLayout.NORTH, play, (getHeight()-350)/2-55, SpringLayout.SOUTH, menu);
+        layout.putConstraint(SpringLayout.NORTH, play, (getHeight()-350)/2, SpringLayout.NORTH, getContentPane());
         layout.putConstraint(SpringLayout.WEST, help, (getWidth()-500)/2, SpringLayout.WEST, getContentPane());
         layout.putConstraint(SpringLayout.NORTH, help, 100, SpringLayout.SOUTH, play);
     }
-    public void setMap() {
+    public void play() {
+        play.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent a) {
+                dispose();
+                try {
+                    dispose();
+                    Game game = new Game();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+    public void help() {}
+
+    public void setMap() throws IOException {
         File tiles = new File("Images/Tiles.png");
+        BufferedImage TILES = ImageIO.read(tiles);
+        int tilesWidth = TILES.getWidth();
+        int tilesHeight = TILES.getHeight();
+        pixels = new int[tilesWidth*tilesHeight];
+        TILES.getRGB(0, 0, tilesWidth, tilesHeight, pixels, 0, tilesWidth);
     }
     public void setTiles(int x, int y) {
 
     }
     public static void main (String[] args) {
-        Game game = new Game();
+        Startup startup = new Startup();
     }
 
 }
