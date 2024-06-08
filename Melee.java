8becebfb05d3bb7431be6de022b585629cf8b95f
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-08 16:00:01 -0400

    Update Melee.java

--- Melee.java
+++ Melee.java
@@ -1,29 +1,46 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
 
-public class Melee extends Player {
+public class Melee extends Player implements KeyListener {
     private int code;
-    public Melee() {
-        super();
-        movement();
+    private Image melee;
+    public Melee(JFrame frame) throws IOException {
+        super(frame);
+        System.out.println("Melee spawned");
+        addKeyListener(this);
+        setFocusable(true);
+        melee = ImageIO.read(new File("Images/Melee/Shuriken.png"));
     }
     public void keyPressed(KeyEvent key) {
         code = key.getKeyCode();
-        if (code == key.VK_W) forward = true;
-        if (code == key.VK_S) back = true;
-        if (code == key.VK_A) left = true;
-        if (code == key.VK_D) right = true;
-        if (code == key.VK_Q) ultimate = true;
-        if (code == key.VK_E) skill = true;
-        if (code == key.VK_SPACE) normal = true;
+        if (code == KeyEvent.VK_W) forward = true;
+        if (code == KeyEvent.VK_S) back = true;
+        if (code == KeyEvent.VK_A) left = true;
+        if (code == KeyEvent.VK_D) right = true;
+        if (code == KeyEvent.VK_Q) ultimate = true;
+        if (code == KeyEvent.VK_E) skill = true;
+        if (code == KeyEvent.VK_SPACE) normal = true;
     }
     public void keyReleased(KeyEvent key) {
         code = key.getKeyCode();
-        if (code == key.VK_W) forward = false;
-        if (code == key.VK_S) back = false;
-        if (code == key.VK_A) left = false;
-        if (code == key.VK_D) right = false;
-        if (code == key.VK_Q) ultimate = false;
-        if (code == key.VK_E) skill = false;
-        if (code == key.VK_SPACE) normal = false;
+        if (code == KeyEvent.VK_W) forward = false;
+        if (code == KeyEvent.VK_S) back = false;
+        if (code == KeyEvent.VK_A) left = false;
+        if (code == KeyEvent.VK_D) right = false;
+        if (code == KeyEvent.VK_Q) ultimate = false;
+        if (code == KeyEvent.VK_E) skill = false;
+        if (code == KeyEvent.VK_SPACE) normal = false;
+    }
+    public void keyTyped(KeyEvent e) {
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(melee, (int)xPos, (int)yPos, 64, 64, null);
     }
 }
