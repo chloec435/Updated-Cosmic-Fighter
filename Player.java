74a8f3c9a58ecefd0d4df3b95499a1836b7c5fdc
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-08 16:00:11 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,43 +1,29 @@
+import javax.swing.*;
 import java.awt.event.KeyEvent;
-public class Player {
+import java.io.IOException;
+
+public class Player extends JPanel{
     protected boolean forward,back,left,right;
     protected boolean normal,skill,ultimate;
     protected double moveSpeed = 0.5;
     protected float yPos, xPos;
     protected float yDir = 1, xDir = 0;
-    protected int[][] map;
-    public Player(){}
-    public Player(String type, int[][] mapping) {
-        type(type);
-        map = mapping;
+    protected JFrame frame;
+    public Player(JFrame frame) {
+        this.frame = frame;
     }
-    public void type(String type) {
-        if (type == "melee") new Melee();
-        if (type == "ranged") new Ranged();
-        if (type == "support") new Support();
-    }
-    //https://medium.com/@niiicolai/an-introduction-to-movement-in-2d-games-281ff3b58533
     public void movement() {
         if (forward) {
-            if (map[ (int) (yPos + yDir * moveSpeed) ][ (int) xPos ] == 0) {
-                yPos += yDir * moveSpeed;
-            }
+            yPos += (float) (yDir * moveSpeed);
         }
         if (back) {
-            if (map[ (int) (yPos - yDir * moveSpeed) ][ (int) xPos ] == 0) {
-                yPos -= yDir * moveSpeed;
-            }
+            yPos -= (float) (yDir * moveSpeed);
         }
         if (left) {
-            if (map[ (int) yPos][ (int) (xPos + xDir * moveSpeed) ] == 0) {
-                xPos += xDir * moveSpeed;
-            }
+            xPos += (float) (xDir * moveSpeed);
         }
         if (right) {
-            if (map[ (int) yPos][ (int) (xPos + xDir * moveSpeed) ] == 0) {
-                xPos -= yDir * moveSpeed;
-            }
+            xPos -= (float) (yDir * moveSpeed);
         }
     }
-
 }
