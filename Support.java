b636e897eca2c0d2a4ac2ddad49a3b24f9b80060
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-08 23:50:12 -0400

    Update Support.java

--- Support.java
+++ Support.java
@@ -3,11 +3,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
-public class Support extends Player implements KeyListener {
+public class Support extends Player {
     private int code;
     private Image support;
     public Support(JFrame frame) throws IOException {
@@ -16,31 +15,53 @@ public class Support extends Player implements KeyListener {
         addKeyListener(this);
         setFocusable(true);
         support = ImageIO.read(new File("Images/Support/Support.png"));
+        setIcon(new ImageIcon(new ImageIcon(support).getImage().
+                getScaledInstance(128, 128, Image.SCALE_SMOOTH)));
+        setSize(128, 128);
+        setLocation((int) xPos, (int) yPos);
+        setVisible(true);
     }
-    public void keyPressed(KeyEvent key) {
-        code = key.getKeyCode();
-        if (code == KeyEvent.VK_KP_UP) forward = true;
-        if (code == KeyEvent.VK_KP_DOWN) back = true;
-        if (code == KeyEvent.VK_KP_LEFT) left = true;
-        if (code == KeyEvent.VK_KP_RIGHT) right = true;
-        if (code == KeyEvent.VK_J) ultimate = true;
-        if (code == KeyEvent.VK_K) skill = true;
-        if (code == KeyEvent.VK_L) normal = true;
+    @Override
+    public void keyPressed(KeyEvent e) {
+        handleKeyPressed(e);
     }
-    public void keyReleased(KeyEvent key) {
-        code = key.getKeyCode();
-        if (code == KeyEvent.VK_KP_UP) forward = false;
-        if (code == KeyEvent.VK_KP_DOWN) back = false;
-        if (code == KeyEvent.VK_KP_LEFT) left = false;
-        if (code == KeyEvent.VK_KP_RIGHT) right = false;
-        if (code == KeyEvent.VK_J) ultimate = false;
-        if (code == KeyEvent.VK_K) skill = false;
-        if (code == KeyEvent.VK_L) normal = false;
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        handleKeyReleased(e);
     }
+
+    @Override
     public void keyTyped(KeyEvent e) {
+        handleKeyTyped(e);
+    }
+    public void handleKeyPressed(KeyEvent key) {
+        code = key.getKeyCode();
+        if (code == KeyEvent.VK_UP) forward = true;
+        if (code == KeyEvent.VK_DOWN) back = true;
+        if (code == KeyEvent.VK_LEFT) left = true;
+        if (code == KeyEvent.VK_RIGHT) right = true;
+        if (code == KeyEvent.VK_NUMPAD3) ultimate = true;
+        if (code == KeyEvent.VK_NUMPAD2) skill = true;
+        if (code == KeyEvent.VK_NUMPAD1) normal = true;
+    }
+    public void handleKeyReleased(KeyEvent key) {
+        code = key.getKeyCode();
+        if (code == KeyEvent.VK_UP) forward = false;
+        if (code == KeyEvent.VK_DOWN) back = false;
+        if (code == KeyEvent.VK_LEFT) left = false;
+        if (code == KeyEvent.VK_RIGHT) right = false;
+        if (code == KeyEvent.VK_NUMPAD3) ultimate = false;
+        if (code == KeyEvent.VK_NUMPAD2) skill = false;
+        if (code == KeyEvent.VK_NUMPAD1) normal = false;
+    }
+    public void handleKeyTyped(KeyEvent e) {
+    }
+    public void movement() {
+        super.movement();
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(support, (int)xPos, (int)yPos, 64, 64, null);
+        g.drawImage(support, (int)xPos, (int)yPos, 128, 128, null);
     }
 }
