30dc1c44892bc20e5c7e974c31a431a69b1c87ab
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-09 02:28:44 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -9,11 +12,32 @@ public abstract class Player extends JLabel implements KeyListener {
     protected float yPos = 10, xPos = 10;
     protected float yDir = 1, xDir = 1;
     protected JFrame frame;
+    protected Image currentImage;
+    protected Image image1;
+    protected Image image2;
+    private Timer animation;
+    private boolean switchImage = false;
     public Player(JFrame frame) {
         this.frame = frame;
         this.frame.requestFocusInWindow();
         setOpaque(false);
         setLayout(null);
+        setSize(frame.getWidth(), frame.getHeight());
+        setLocation((int) xPos, (int) yPos);
+        setVisible(true);
+    }
+    public void image() {
+        currentImage = image1;
+        animation = new Timer(1000/2, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchImage = !switchImage;
+                if (switchImage) currentImage = image1;
+                else currentImage = image2;
+                repaint();
+            }
+        });
+        animation.start();
     }
     public void movement() {
         if (forward && yPos != 0) {
