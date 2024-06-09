9e6c05979fdb6ca9095d8ef3868fa8080dfaf851
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-09 02:28:55 -0400

    Update Melee.java

--- Melee.java
+++ Melee.java
@@ -1,25 +1,43 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Image;
+//import java.awt.Image;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
 public class Melee extends Player {
     private int code;
-    private Image melee;
+//    private Image melee1;
+//    private Image melee2;
+//    private Image currentImage;
+//    private Timer animation;
+//    private boolean switchImage = false;
+
     public Melee(JFrame frame) throws IOException {
         super(frame);
         System.out.println("Melee spawned");
         addKeyListener(this);
         setFocusable(true);
-        melee = ImageIO.read(new File("Images/Melee/Shuriken.png"));
-        setIcon(new ImageIcon(new ImageIcon(melee).getImage().
-                getScaledInstance(128, 128, Image.SCALE_SMOOTH)));
-        setSize(128, 128);
-        setLocation((int) xPos, (int) yPos);
-        setVisible(true);
+        image1 = ImageIO.read(new File("Images/Melee/Shuriken.png"));
+        image2 = ImageIO.read(new File("Images/Melee/Shuriken (1).png"));
+        image();
+//        currentImage = melee1;
+//        setSize(frame.getWidth(), frame.getHeight());
+//        setLocation((int) xPos, (int) yPos);
+//        setVisible(true);
+//        animation = new Timer(1000/12, new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                switchImage = !switchImage;
+//                if (switchImage) currentImage = melee1;
+//                else currentImage = melee2;
+//                repaint();
+//            }
+//        });
+//        animation.start();
     }
     @Override
     public void keyPressed(KeyEvent e) {
@@ -62,6 +80,7 @@ public class Melee extends Player {
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(melee, (int)xPos, (int)yPos, 128, 128, null);
+        System.out.println("Repainting component");
+        g.drawImage(currentImage, (int)xPos, (int)yPos, 128, 128, null);
     }
 }
