de267c34c0067fde1753c34be6da4fdc39aea33a
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-08 23:49:29 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,5 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.Image;
 import java.awt.event.*;
@@ -13,16 +12,17 @@ public class Game {
     private BufferedImage background;
     private int clicks = 0;
     private JPanel backgroundPanel;
-    private JPanel gamePanel;
+    private JPanel gameBackgroundPanel;
     private String playerOne;
     private String playerTwo;
     private JFrame frame;
+    private JFrame game;
     private Player one;
     private Player two;
     private ArrayList<Player> players = new ArrayList<Player>();
     public Game() throws IOException {
-        frame = new JFrame();
         frame();
+        selection();
         frame.setVisible(true);
         frame.addWindowListener(new java.awt.event.WindowAdapter()  {
             @Override
@@ -32,13 +32,11 @@ public class Game {
         });
     }
     public void frame() throws IOException {
+        frame = new JFrame();
         frame.setSize(1920, 1080);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        selection();
-    }
-    public void selection() throws IOException {
         background = ImageIO.read(new File("Images/ufo-1265186_1920.jpg"));
         backgroundPanel = new JPanel() {
             @Override
@@ -48,7 +46,9 @@ public class Game {
             }
         };
         backgroundPanel.setPreferredSize(new Dimension(1920, 1080));
-
+        backgroundPanel.setLayout(null);
+    }
+    public void selection() {
         JLabel melee = new JLabel(new ImageIcon(new ImageIcon("Images/Melee/Shuriken.png")
                 .getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH)));
         JLabel ranged = new JLabel(new ImageIcon(new ImageIcon("Images/Ranged/Ranged.png")
@@ -57,26 +57,17 @@ public class Game {
                 .getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH)));
         JLabel choose = new JLabel(new ImageIcon(new ImageIcon("Images/Words/choose char.png")
                 .getImage().getScaledInstance(1600, 150, Image.SCALE_SMOOTH)));
+
+        melee.setBounds(100, 300, 500, 500);
+        ranged.setBounds(700, 300, 500, 500);
+        support.setBounds(1300, 300, 500, 500);
+        choose.setBounds((frame.getWidth() - 1600) / 2, 100, 1600, 150);
+
         backgroundPanel.add(melee);
         backgroundPanel.add(ranged);
         backgroundPanel.add(support);
         backgroundPanel.add(choose);
-
-        SpringLayout layout = new SpringLayout();
-        backgroundPanel.setLayout(layout);
         frame.setContentPane(backgroundPanel);
-        int width = (frame.getWidth() - 1500) / 4;
-        int height = (frame.getHeight() - 450) / 2;
-
-        layout.putConstraint(SpringLayout.WEST, choose, (frame.getWidth() - 1600) / 2, SpringLayout.WEST, backgroundPanel);
-        layout.putConstraint(SpringLayout.NORTH, choose, 100, SpringLayout.NORTH, backgroundPanel);
-        layout.putConstraint(SpringLayout.WEST, melee, width, SpringLayout.WEST, backgroundPanel);
-        layout.putConstraint(SpringLayout.NORTH, melee, height, SpringLayout.NORTH, choose);
-        layout.putConstraint(SpringLayout.WEST, ranged, width, SpringLayout.EAST, melee);
-        layout.putConstraint(SpringLayout.NORTH, ranged, height, SpringLayout.NORTH, choose);
-        layout.putConstraint(SpringLayout.WEST, support, width, SpringLayout.EAST, ranged);
-        layout.putConstraint(SpringLayout.NORTH, support, height, SpringLayout.NORTH, choose);
-
         melee.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -126,41 +117,83 @@ public class Game {
             playerTwo = type;
             JOptionPane.showMessageDialog(backgroundPanel, "Player 2 will be " + type + ".",
                     "Player Two", JOptionPane.INFORMATION_MESSAGE);
-            backgroundPanel.removeAll();
-            frame.revalidate();
-            frame.repaint();
-            spawnCharacters();
+            frame.dispose();
+            gameFrame();
         }
     }
-    public void spawnCharacters() throws IOException {
+    public void gameFrame() throws IOException {
+        game = new JFrame();
+        game.setSize(1920, 1080);
+        game.setResizable(false);
+        game.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        game.setLocationRelativeTo(null);
+        gameBackgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        gameBackgroundPanel.setPreferredSize(new Dimension(1920, 1080));
+        gameBackgroundPanel.setLayout(null);
+        game.setVisible(true);
+        game.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                Startup main = new Startup();
+            }
+        });
         switch (playerOne) {
-            case "melee" -> one = new Melee(frame);
-            case "ranged" -> one = new Ranged(frame);
-            case "support" -> one = new Support(frame);
+            case "melee" -> one = new Melee(game);
+            case "ranged" -> one = new Ranged(game);
+            case "support" -> one = new Support(game);
         }
         switch (playerTwo) {
-            case "melee" -> two = new Melee(frame);
-            case "ranged" -> two = new Ranged(frame);
-            case "support" -> two = new Support(frame);
+            case "melee" -> two = new Melee(game);
+            case "ranged" -> two = new Ranged(game);
+            case "support" -> two = new Support(game);
         }
+        game.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                for (Player player : players) {
+                    player.keyPressed(e);
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                for (Player player : players) {
+                    player.keyReleased(e);
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                for (Player player : players) {
+                    player.keyTyped(e);
+                }
+            }
+        });
+
         players.add(one);
         players.add(two);
-        backgroundPanel.add(one);
-        backgroundPanel.add(two);
-        frame.revalidate();
-        frame.repaint();
+        gameBackgroundPanel.add(one);
+        gameBackgroundPanel.add(two);
+        game.setContentPane(gameBackgroundPanel);
+        gameBackgroundPanel.revalidate();
+        gameBackgroundPanel.repaint();
+
         gameLoop();
     }
     public void gameLoop() {
-        if (!frame.isShowing()) {
-            System.out.println("ended");
+        if (!game.isShowing()) {
             return;
         }
         for (Player player : players) {
             player.movement();
-            System.out.println("running");
         }
-        gamePanel.repaint();
+        gameBackgroundPanel.repaint();
         Timer timer = new Timer(10, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
