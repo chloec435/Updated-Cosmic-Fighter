3296f2dadae650f2afb4fbe6ca7fefbec556f267
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-06-09 20:34:19 -0400

    Update Ranged.java

--- Ranged.java
+++ Ranged.java
@@ -1,20 +1,15 @@
 import javax.imageio.ImageIO;
-import javax.swing.*;
-//import java.awt.Image;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ranged extends Character {
     private int code;
     private int normalDmg = 15, skillDmg = 40;
-//    private Image ranged1;
-//    private Image ranged2;
-//    private Image currentImage;
-//    private Timer animation;
-//    private boolean switchImage = false;
     public Ranged(JFrame frame) throws IOException {
         super(frame);
         System.out.println("Ranged spawned");
@@ -26,20 +21,6 @@ public class Ranged extends Character {
         skill1 = ImageIO.read(new File("Images/Ranged/Ranged AOE.png"));
         skill2 = ImageIO.read(new File("Images/Ranged/Ranged AOE (1).png"));
         animate(normal, normal, 1);
-//        currentImage = ranged1;
-//        setSize(frame.getWidth(), frame.getHeight());
-//        setLocation((int) xPos, (int) yPos);
-//        setVisible(true);
-//        animation = new Timer(1000/2, new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                switchImage = !switchImage;
-//                if (switchImage) currentImage = ranged1;
-//                else currentImage = ranged2;
-//                repaint();
-//            }
-//        });
-//        animation.start();
     }
     @Override
     public void keyPressed(KeyEvent e) {
@@ -84,6 +65,7 @@ public class Ranged extends Character {
             delayFromSkill(normal, normal, 2000,1);
         }
         if (code == KeyEvent.VK_P) {
+            attack = false;
             delayFromAttack(normal, normal, 2000,1);
         }
     }
@@ -92,8 +74,4 @@ public class Ranged extends Character {
     public void movement() {
         super.movement();
     }
-//    public void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//        g.drawImage(currentImage, (int)xPos, (int)yPos, 128, 128, null);
-//    }
 }
